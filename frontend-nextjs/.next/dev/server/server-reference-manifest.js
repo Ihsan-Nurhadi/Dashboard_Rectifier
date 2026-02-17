d399ec5cc915b662f28b33f70ff137bc6f85a6a0
Naschi <naschivnr28@gmail.com>
2026-02-17 11:52:28 +0700

    Add .gitignore - remove build artifacts from tracking

--- frontend-nextjs/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/lFtpUgPQTgDzJKKTjsrEgm+EpU+J6razzDUU6+Nt7E=\"\n}"
--- frontend-nextjs/.next/dev/static/chunks/src_app_page_tsx_1e2ba041._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_ui_Map_tsx_2646b0fb._.js",
-  "static/chunks/src_cae43305._.js",
-  "static/chunks/node_modules_3f3bfaec._.js"
-],
-    source: "dynamic"
-});
